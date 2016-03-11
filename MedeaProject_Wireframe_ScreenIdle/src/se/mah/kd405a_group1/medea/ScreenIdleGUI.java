bb036820b80fc25d6c681c1bc6f1e1bf64993137
Johan Anderson <johan@obsolete.se>
2016-03-11 09:27:40 +0100

    Added idle screen wireframe project.

---
+++ MedeaProject_Wireframe_ScreenIdle/src/se/mah/kd405a_group1/medea/ScreenIdleGUI.java
@@ -0,0 +1,66 @@
+package se.mah.kd405a_group1.medea;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import javax.swing.ImageIcon;
+
+public class ScreenIdleGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ScreenIdleGUI frame = new ScreenIdleGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ScreenIdleGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		System.out.println("JFrame Width: "+width+" Height: "+height);
+		this.setBounds(0, 0, (int)width, (int)height); 
+		this.setUndecorated(true);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.PINK);
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(ScreenIdleGUI.class.getResource("/se/mah/kd405a_group1/medea/res/wireframe.png")));
+		lblNewLabel.setBounds(0, 0, (int)width, (int)height);
+		contentPane.add(lblNewLabel);
+	}
+}
