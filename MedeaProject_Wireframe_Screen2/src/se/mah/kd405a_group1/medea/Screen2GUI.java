2ac964c4aa729a371a329be71f681375c763b6df
Johanna Johnson <johannaevasofia@gmail.com>
2016-03-16 15:25:21 +0100

    hej

--- MedeaProject_Wireframe_Screen2/src/se/mah/kd405a_group1/medea/Screen2GUI.java
+++ MedeaProject_Wireframe_Screen2/src/se/mah/kd405a_group1/medea/Screen2GUI.java
@@ -58,10 +58,18 @@ public class Screen2GUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		// Start screen pic
 		JLabel lblNewLabel2 = new JLabel("");
 		lblNewLabel2.setIcon(new ImageIcon(Screen2GUI.class.getResource("/se/mah/kd405a_group1/medea/res/MedeaStart.png")));
-		lblNewLabel2.setBounds(0, 0, (int)width, (int)height);
+		lblNewLabel2.setBounds(6, 19, (int)width, (int)height);
 		contentPane.add(lblNewLabel2);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(0, 0, 1286, 800);
+		contentPane.add(lblNewLabel);
+		
+		//JLabel lblNewLabel = new JLabel("");
+		//lblNewLabel.setIcon(new ImageIcon(Screen2GUI.class.getResource("/se/mah/kd405a_group1/medea/res/wr_3-01.jpg")));
+		//lblNewLabel.setBounds(0, 0, (int)width, (int)height);
+		//contentPane.add(lblNewLabel);
 	}
 }
