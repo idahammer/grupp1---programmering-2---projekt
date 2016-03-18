45b085f11bc098f50ad618cd403ecddec4838812
Johan Anderson <johan@obsolete.se>
2016-03-18 13:03:37 +0100

    Moved arrow

--- MedeaProject_Wireframe_Screen3/src/se/mah/kd405a_group1/medea/Screen3GUI.java
+++ MedeaProject_Wireframe_Screen3/src/se/mah/kd405a_group1/medea/Screen3GUI.java
@@ -63,21 +63,25 @@ public class Screen3GUI extends JFrame {
 		contentPane.setLayout(null);
 		
 		// Start screen pic
-		JLabel lblNewLabel2 = new JLabel("");
+		/*JLabel lblNewLabel2 = new JLabel("");
 		lblNewLabel2.setIcon(new ImageIcon(Screen3GUI.class.getResource("/se/mah/kd405a_group1/medea/res/MedeaStart.png")));
 		lblNewLabel2.setBounds(0, 0, (int)width, (int)height);
-		contentPane.add(lblNewLabel2);
+		contentPane.add(lblNewLabel2);*/
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(Screen3GUI.class.getResource("/se/mah/kd405a_group1/medea/res/pil2.gif")));
-		lblNewLabel.setBounds(0, 0, (int)width, (int)height);
+		int iconW = lblNewLabel.getIcon().getIconWidth();
+		int iconH = lblNewLabel.getIcon().getIconHeight();
+		int iconX = ((int)width / 2) - (iconW / 2);
+		int iconY = ((int)height / 2) - (iconH / 2);
+		lblNewLabel.setBounds(iconX, iconY, iconW, iconH);
 		contentPane.add(lblNewLabel);
 		
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
 			@Override
 			public boolean dispatchKeyEvent(KeyEvent e) {
 				
-				lblNewLabel2.setIcon(new ImageIcon(Screen3GUI.class.getResource("/se/mah/kd405a_group1/medea/res/Medea3jpg.jpg"))); //byt till bild
+				//lblNewLabel2.setIcon(new ImageIcon(Screen3GUI.class.getResource("/se/mah/kd405a_group1/medea/res/Medea3jpg.jpg"))); //byt till bild
 				lblNewLabel.setIcon(null); // tar bort pilen
 				System.out.println("Got key event!");
 				return false;
