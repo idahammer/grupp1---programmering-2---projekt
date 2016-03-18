2bd99183d1c09cb3d40ab3d7636e227461a42ab8
idahammer <ida.hammer@gmail.com>
2016-03-18 08:41:14 +0100

    added to screen1

--- MedeaProject_Wireframe_Screen1/src/se/mah/kd405a_group1/medea/Screen1GUI.java
+++ MedeaProject_Wireframe_Screen1/src/se/mah/kd405a_group1/medea/Screen1GUI.java
@@ -97,7 +97,7 @@ public class Screen1GUI extends JFrame  {
 		
 		// Start screen pic
 		JLabel lblNewLabel2 = new JLabel("");
-		//lblNewLabel2.setIcon(new ImageIcon(Screen1GUI.class.getResource("/se/mah/kd405a_group1/medea/res/MedeaStart.png")));
+		lblNewLabel2.setIcon(new ImageIcon(Screen1GUI.class.getResource("/se/mah/kd405a_group1/medea/res/MedeaStart.png")));
 		lblNewLabel2.setBounds(0, 0, 1080, 1920);
 		contentPane.add(lblNewLabel2);
 		
